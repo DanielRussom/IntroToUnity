6d35629c31a717ed3bd38bc4ef3733a8b2f42bc0
Daniel Russom <daniel.russom00@gmail.com>
2023-02-20 11:25:20 +0000

    Prevent double jumping

--- Assets/3D Platformer Game/Scripts/PlayerController.cs
+++ Assets/3D Platformer Game/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    private bool isGrounded;
+
     void Update()
     {
         var x = GetAxisVelocity("Horizontal");
@@ -13,9 +15,18 @@ public class PlayerController : MonoBehaviour
 
         player.velocity = new Vector3(x, player.velocity.y, z);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            isGrounded = false;
             player.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.GetContact(0).normal == Vector3.up)
+        {
+            isGrounded = true;
         }
     }
 
