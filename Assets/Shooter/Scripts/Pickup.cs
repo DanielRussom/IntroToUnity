5eb1a5f55fe056434b5792d460e70410cda398de
Daniel Russom <daniel.russom00@gmail.com>
2023-05-20 20:35:04 +0100

    Powerups float up and down

--- Assets/Shooter/Scripts/Pickup.cs
+++ Assets/Shooter/Scripts/Pickup.cs
@@ -10,6 +10,32 @@ public class Pickup : MonoBehaviour
     public int value;
     public PickupType type;
 
+    [Header("animation")]
+    public float rotateSpeed;
+    public float floatSpeed;
+    public float floatHeight;
+
+    private Vector3 startPosition;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        offset = new Vector3(0, floatHeight, 0);
+    }
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, startPosition + offset, floatSpeed * Time.deltaTime);
+
+        if(transform.position == startPosition + offset)
+        {
+            offset *= -1;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -28,4 +54,6 @@ public class Pickup : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+
 }
